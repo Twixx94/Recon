9e8a40c77771ed91dbce64bd5ce71d429f974824
Thomas <thomas.rc.fr@gmail.com>
2022-12-13 19:12:29 +0100

    Recon V2.2

--- API/controller/user.js
+++ API/controller/user.js
@@ -1,5 +1,6 @@
 const { User } = require("../model/user")
 const client  = require('../database/connect');
+const { ObjectID } = require("bson");
 
 const addUser = async (req, res)=>{
     try{
@@ -19,7 +20,7 @@ const addUser = async (req, res)=>{
     }
 };
 
-const getUser = async (req, res)=>{
+const getAllUser = async (req, res)=>{
     try{
     let cursor = client.db().collection("user").find();
     let result = await cursor.toArray();
@@ -34,4 +35,48 @@ const getUser = async (req, res)=>{
     }
 };
 
-module.exports = {addUser, getUser};
+const getUser = async (req, res)=>{
+    try{
+    let id = new ObjectID(req.params.id);
+    let cursor = client.db().collection("user").find({_id : id});
+    let result = await cursor.toArray();
+    if(result.length>0){
+        res.status(200).json(result[0]);
+    }else{
+        res.status(204).json({msg : "Ce user n'existe pas"});
+    }
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error);
+    }
+};
+
+const editUser = async (req, res)=>{
+    try{
+        let id = new ObjectID(req.params.id);
+        let newLogin = req.body.login;
+        let newPassword = req.body.password;
+
+        let result = await client.db().collection("user").updateOne(
+            {_id : id}, 
+            {$set : {login : newLogin, password : newPassword}});
+            res.status(200).json(result);
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+
+const deleteUser = async (req, res)=>{
+    try{
+        let id = new ObjectID(req.params.id);
+
+        let result = await client.db().collection("user").deleteOne(
+            {_id : id});
+            res.status(200).json(result);
+    }catch(error){
+        console.log(error);
+        res.status(500).json(error);
+    }
+}
+module.exports = {addUser, getAllUser, getUser, deleteUser, editUser};
--- API/route/user.js
+++ API/route/user.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { addUser, getUser } = require('../controller/user');
+const { addUser, getAllUser, getUser, editUser, deleteUser } = require('../controller/user');
 const router = express.Router();
 
 router.route("/user").post(addUser);
-router.route("/user").get(getUser);
+router.route("/user").get(getAllUser);
+router.route("/user/:id").get(getUser);
+router.route("/user/:id").put(editUser);
+router.route("/user/:id").delete(deleteUser);
 
 module.exports = router;
